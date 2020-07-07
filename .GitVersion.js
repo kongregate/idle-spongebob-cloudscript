350e6cb6c7402f90a0abf3cb6230bea18f364daa
Jeremy Brown <jbrown@ultrabit.com>
2020-07-07 16:25:42 -0700

    Cleaned up CloudRequest methods

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,5 +1,5 @@
 /*
-169bc791546ac8979bdac332f40a65c916f1b943
+601a8806ba859cb9cf4e4f945e7a1eaaf4ec53fb
 jbrown/20200708-cleanup
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
