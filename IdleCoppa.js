821baa8adbcd84bf4d81e69a9d863f6536d86b59
Luis Saenz <lsaenz@ultrabit.com>
2020-09-08 16:53:59 -0700

    adding coppa file to set coppa model in player readonly section

---
+++ IdleCoppa.js
@@ -0,0 +1,45 @@
+
+handlers.setAndGetCoppaModel = function(args) {
+	var result = {
+        "success": false,
+        "coppa" : null
+	};
+
+    if (args.birthdayTimestamp == null
+        || args.birthdayTimestamp == undefined
+    ) {
+        return result;
+    }
+
+    result.coppa["birthdayTimestamp"] = args.birthdayTimestamp;
+
+    var countryCode = null;
+    try {
+        var playerProfile = server.GetPlayerProfile({
+            "PlayFabId" : currentPlayerId,
+            "ProfileConstraints" : args.constrains
+        });
+
+        countryCode = playerProfile.Locations[0].CountryCode;
+        result.coppa["countryISO3166Code"] = countryCode;
+    } catch(e) {
+        result["error"] = e;
+        return { "value" : result };
+    }
+
+    try {
+        server.UpdateUserReadOnlyData({
+            "PlayFabId" : currentPlayerId,
+            "Data" : {
+                "coppa" : result.coppa
+            }
+        });
+    } catch(e) {
+        result["error"] = e;
+        return { "value" : result };
+    }
+
+    result.success = true;
+    return { "value" : result };
+
+};
