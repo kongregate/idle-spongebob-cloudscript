76775d5513e55d1e742c87f738e054a2ed09f723
Luis Saenz <lsaenz@ultrabit.com>
2020-09-09 14:20:24 -0700

    bumping version

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,6 +1,6 @@
 /*
-7419a5c183256b87c80e874040b675e616326fc1
-jbrown/20200708-cleanup
+6b3e207744819c83acb9a17137786eb54ccc3f15
+lsaenz/coppa
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
 */
