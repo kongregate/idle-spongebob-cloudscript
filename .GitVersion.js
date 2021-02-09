e84c2dca49c3837dd1759c48fb9b3ee0dd1c496a
LuisUltrabit <30806931+LuisUltrabit@users.noreply.github.com>
2021-02-09 11:09:56 -0800

    Lsaenz/coppa (#5)
    * updating coppa function to write model without returning it
    
    * setting coppa adult consent from args
    
    * updating coppa model in user readonly
    
    * removing commented code

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,6 +1,6 @@
 /*
-25a9ea9c00d8518406cb1f967ec3436d52123545
-master
+d9fad3ddffedcf609c3de6534d18ed59c589ff5a
+lsaenz/coppa
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
 */
--- IdleCoppa.js
+++ IdleCoppa.js
@@ -1,34 +1,16 @@
 
 const COPPA = "coppa";
 
-handlers.setAndGetCoppaModel = function(args) {
+handlers.setCoppaModel = function(args) {
 	var result = {
         "success": false,
         "saved" : false
-	};
+    };
 
-    if (args.birthdayTimestamp == null
-        || args.birthdayTimestamp == undefined
-    ) {
-        return result;
-    }
-
-    result[COPPA] = {};
-    result[COPPA]["birthdayTimestamp"] = args.birthdayTimestamp;
-
-    var countryCode = null;
-    try {
-        var playerProfileResponse = server.GetPlayerProfile({
-            "PlayFabId" : currentPlayerId,
-            "ProfileConstraints" : args.constrains
-        });
-
-        countryCode = playerProfileResponse.PlayerProfile.Locations[0].CountryCode;
-        result[COPPA]["countryCode"] = countryCode;
-    } catch(e) {
-        result["error"] = evaluatePlayFabError(e);
-        return { "value" : result };
-    }
+    result[COPPA] = {
+        "hasAdultConsent": (args.hasAdultConsent === true),
+        "isMinor": (args.isMinor === true)
+    };
 
     try {
         var data = {};
