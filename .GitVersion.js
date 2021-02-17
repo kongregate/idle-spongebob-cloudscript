a59f2bb38623ba09a703db912a15489001a5bb70
Luis Saenz <lsaenz@ultrabit.com>
2021-02-17 14:49:55 -0800

    bumping version

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,5 +1,5 @@
 /*
-6687d5568c1af361a0f798c5a6f58f54c203b32e
+54745f0322f0b8a7ae9d4725714a49cd16738546
 master
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
