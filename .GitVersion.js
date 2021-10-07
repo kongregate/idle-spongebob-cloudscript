1e7b4240aa02c6c369e01d06fed5800f34070998
Luis Saenz <lsaenz@ultrabit.com>
2021-10-07 14:37:28 -0700

    moving current banning functions into their own file

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,5 +1,5 @@
 /*
-fa8120a16f90afbbf7081fb21912fba5a9bef984
+72f0a105006f9fa31c61b8945307b66cc0e7f466
 lsaenz/banning-internally
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
