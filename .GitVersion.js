bf6aeb205c4e4c01b8e4bc97a766cf3a005b36a3
Luis Saenz <lsaenz@ultrabit.com>
2021-10-11 16:57:34 -0700

    fixing check for existing data

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,5 +1,5 @@
 /*
-e9de5a3fe8f7bbe34f293359b0dba0a333f7c649
+3936656baa389beb20f94b2e5ae8d48bfe85dfe7
 lsaenz/banning-get-isCheater
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
