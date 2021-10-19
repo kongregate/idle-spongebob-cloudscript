a184cd240b4ef295b97373a713c667ee916f4156
Luis Saenz <lsaenz@ultrabit.com>
2021-10-19 16:50:08 -0700

    bumping version

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,6 +1,6 @@
 /*
-3aa4b6fac35dae78ca9edb9ed88120e100aec48d
-lsaenz/cheater-leaderboards
+012a7118b00aac69b7d18af56ad57bd9c65e81d0
+master
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
 */
