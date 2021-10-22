81517b0214fbac3437ca5b6dcb52eb43870d0d19
Luis Saenz <lsaenz@ultrabit.com>
2021-10-22 15:35:22 -0700

    bumping version

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,6 +1,6 @@
 /*
-0dae5cdc5b95c5d6ee1372838ab59ee5cd423ca3
-lsaenz/reserved-leaderboard-positions
+8fe69dece7c810c71caf4c16b0d7683dfbfa4cfd
+master
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
 */
