70c7ec25c9cfef4bdb8c3f9a88dc91a0f1328e00
Luis Saenz <lsaenz@ultrabit.com>
2022-01-19 12:05:14 -0800

    bumping cloud script version

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,6 +1,6 @@
 /*
-f4d501ca0633399a2bc3074b8ba2390db59c20c1
-lsaenz/player-account-removal
+832bf713ab09b4961f4a29645b14ca2d8a5a8d4b
+master
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
 */
