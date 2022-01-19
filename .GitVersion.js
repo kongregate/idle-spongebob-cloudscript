92bcd819e0ec68372d1b8fbeb1a3a5ccc4fd34bc
Luis Saenz <lsaenz@ultrabit.com>
2022-01-19 10:52:10 -0800

    adding client/admin api to remove/delete playfab account

--- .GitVersion.js
+++ .GitVersion.js
@@ -1,6 +1,6 @@
 /*
-b0a296bc26a82901f7f65775a8076dc5a0add488
-master
+3d40a7b2f604f37acf39fe9f01d1886ff0479bf2
+lsaenz/player-account-removal
 IMPORTANT:
 The above value is not directly mappable to a commit in the repo, but can be used to confirm that the same Cloudscript file is in place between Development and Production
 */
---
+++ IdlePlayerAccountRemoval.js
@@ -0,0 +1,30 @@
+
+handlers.deleteMyAccount = function(args) {
+    var result = {};
+
+    result['delete'] = deletePlayerAccountInternal(currentPlayerId);
+
+    result["success"] = true;
+
+    return {
+        "value": result
+    }
+}
+
+handlers.deletePlayerAccount = function(args) {
+    var result = {};
+
+    result['delete'] = deletePlayerAccountInternal(args.playerId);
+
+    result["success"] = true;
+
+    return {
+        "value": result
+    }
+}
+
+var deletePlayerAccountInternal = function(playerId) {
+    return server.DeletePlayer({
+        "PlayFabId":playerId
+    });
+}
